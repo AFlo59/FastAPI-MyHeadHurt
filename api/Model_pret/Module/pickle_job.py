36bbf815725bc0b59f3f04a1ea36d4527d6a6392
AFlo59 <etudessup59230@gmail.com>
2024-02-23 18:04:24 +0100

    up

--- api/Model_pret/Module/pickle_job.py
+++ api/Model_pret/Module/pickle_job.py
@@ -9,7 +9,10 @@ def sauvegarder_modele(model, nom_fichier):
         model: Le modèle à sauvegarder.
         nom_fichier (str): Le nom du fichier où sauvegarder le modèle.
     """
-    chemin_fichier = os.path.join("..", "Model", nom_fichier + ".pkl")
+    chemin_dossier = os.path.join("..", "Model")
+    if not os.path.exists(chemin_dossier):
+        os.makedirs(chemin_dossier)
+    chemin_fichier = os.path.join(chemin_dossier, nom_fichier + ".pkl")
     with open(chemin_fichier, 'wb') as f:
         pickle.dump(model, f)
     print(f"Le modèle a été sauvegardé dans '{chemin_fichier}'.")
@@ -24,7 +27,14 @@ def charger_modele(nom_fichier):
     Returns:
         model: Le modèle chargé.
     """
-    chemin_fichier = os.path.join("..", "Model", nom_fichier + ".pckl")
+    chemin_dossier = os.path.join("..", "Model")
+    if not os.path.exists(chemin_dossier):
+        print(f"Le dossier '{chemin_dossier}' n'existe pas. Aucun modèle trouvé.")
+        return None
+    chemin_fichier = os.path.join(chemin_dossier, nom_fichier + ".pckl")
+    if not os.path.exists(chemin_fichier):
+        print(f"Le fichier '{chemin_fichier}' n'existe pas. Aucun modèle trouvé.")
+        return None
     with open(chemin_fichier, 'rb') as f:
         loaded_model = pickle.load(f)
     print(f"Le modèle a été chargé à partir de '{chemin_fichier}'.")
