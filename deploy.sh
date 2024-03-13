a3c203a7904001317952c9250d5e58145f56328e
SimonRqrt <simon.riquart@icloud.com>
2024-03-13 13:37:37 +0100

    up mercredi midi

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-<<<<<<< HEAD
 az container create --resource-group RG_RIQUARTS --file deploy.yaml
-=======
-az container create --resource-group RG_ABADIF --file deploy-aci.yaml
->>>>>>> efdc92fe11a7a189cd7006dedc1bab6197657122
