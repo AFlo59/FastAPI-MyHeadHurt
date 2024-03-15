e8e9eb71d5cf68ddf4383fe724875511af15a089
AFlo59 <etudessup59230@gmail.com>
2024-03-15 10:09:30 +0100

    y

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-docker build --platform linux/amd64 -t rickysims/django-sba ./web/.
-docker push rickysims/django-sba
+docker build --platform linux/amd64 -t fabadi/django-sba ./web/.
+docker push fabadi/django-sba
 
-docker build --platform linux/amd64 -t rickysims/api-sba ./api/.
-docker push rickysims/api-sba
+docker build --platform linux/amd64 -t fabadi/api-sba ./api/.
+docker push fabadi/api-sba
 
-az container create --resource-group RG_RIQUARTS --file deploy.yaml
+az container create --resource-group RG_ABADIF --file deploy.yaml
